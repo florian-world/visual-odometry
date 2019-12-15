39d2eda4702f879bff9b0733da0389762e0190cc
simonbing <bings@student.ethz.ch>
2019-12-15 12:24:07 +0100

    removed descriptors and cleaned up code

--- code/bootstrap.m
+++ code/bootstrap.m
@@ -10,10 +10,8 @@ function State = bootstrap(Im1,Im2)
 %       State:  Struct that describes the state containing following
 %               fields:
 %           Keypoints:                   [2xK] array containing K keypoint pairs 
-%           Landmarks:                   [3xK] array containing K landmark coordinates 
-%           Descriptors:                 [256xK] array containing K descriptors of previous frame 
+%           Landmarks:                   [3xK] array containing K landmark coordinates
 %           CandidateKeypoints:          [2xM] array containing M candidate keypoints
-%           CandidateDescriptors:        [256xM] array containing M descriptors of candidate keypoints 
 %           InitCandidateKeypoints:      [2xM] array containing M initial observations of candidate keypoints
 %           InitCandidatePoses:          [12xM] array containing M initial camera poses of first
 %                                         observation of candidate keypoints
@@ -25,17 +23,8 @@ width = imSize(2);
 % Detect corners on both frames
 roi = [PATCHRADIUS+1,PATCHRADIUS+1,width-(2*PATCHRADIUS),height-(2*PATCHRADIUS)];
 corners1 = detectHarrisFeatures(Im1,'ROI',roi);
-corners2 = detectHarrisFeatures(Im2,'ROI',roi);
 
-% Describe Keypoints
-Desc1 = describeKeyPoints(Im1,corners1.Location(:,1),corners1.Location(:,2));
-Desc2 = describeKeyPoints(Im2,corners2.Location(:,1),corners2.Location(:,2));
-
-% Match keypoints by calculating BRIEF descriptors and matching
-[Match1, Match2, Idx1, Idx2] = matchDescriptors(Desc1,Desc2,corners1.Location,corners2.Location);
-Descriptors = Desc2(:,Idx2);
-
-% TEST: Point tracking using KLT
+% Point tracking using KLT
 pointTracker = vision.PointTracker('MaxBidirectionalError',1); % Set to Inf for speedup
 initialize(pointTracker,corners1.Location,Im1);
 
@@ -45,17 +34,12 @@ KLTMatch2 = trackedPoints(trackedPointsValidity,:);
 
 F_KLT = estimateFundamentalMatrix(KLTMatch1, KLTMatch2, 'Method', ...
                               'RANSAC', 'NumTrials', 2000);
-
-% Estimate relative pose between initial frames and create 3D pointcloud
-% Check if det(F) = 0, if not correct as in Ex. 6 (Simon)
-F = estimateFundamentalMatrix(Match1, Match2, 'Method', ...
-                              'RANSAC', 'NumTrials', 2000);
                  
 % Recover essential matrix from F, then decompose into R,T
 E = K'*F_KLT*K;
 [Rots,u3] = decomposeEssentialMatrix(E);
-KLTMatch1(:,3)=1; % TODO: These must be the homogeneous coords of the matches
-KLTMatch2(:,3)=1; % TODO: These must be the homogeneous coords of the matches
+KLTMatch1(:,3)=1;
+KLTMatch2(:,3)=1;
 [R,T] = disambiguateRelativePose(Rots,u3,KLTMatch1',KLTMatch2',K,K);
 
 % Triangulate points to create pointcloud
@@ -64,14 +48,12 @@ M2 = K * [R, T];
 X_hom = linearTriangulation(KLTMatch1',KLTMatch2',M1,M2); % Output of this is homogenous
 Landmarks = X_hom(1:3,:);
 
-Keypoints = KLTMatch2(:,1:2)'; %TODO: Check dimension, also of X                      
+Keypoints = KLTMatch2(:,1:2)';                     
   
 State.Keypoints = Keypoints;
 State.Landmarks = Landmarks;
-State.Descriptors = Descriptors;
 % These state entries are not calculated during bootstrapping
 State.CandidateKeypoints = [];
-State.CandidateDescriptors = [];
 State.InitCandidateKeypoints = [];
 State.InitCandidatePoses = [];
 
--- code/processFrame.m
+++ code/processFrame.m
@@ -11,8 +11,7 @@ function [curState,curPose] = processFrame(prevState,prev_image,image)
 %           Keypoints:                   [2xK] array containing K keypoint pairs
 %           Landmarks:                   [3xK] array containing K landmark coordinates
 %           Descriptors:                 [256xK] array containing K descriptors of previous frame
-%           CandidateKeypoints:          [2xM] array containing M candidate keypoints
-%           CandidateDescriptors:        [256xM] array containing M descriptors of candidate keypoints 
+%           CandidateKeypoints:          [2xM] array containing M candidate keypoints 
 %           InitCandidateKeypoints:      [2xM] array containing M initial observations of
 %                                         candidate keypoints
 %           InitCandidatePoses:          [12xM] array containing M initial camera poses of first
@@ -34,12 +33,6 @@ global K PATCHRADIUS FRAME_NUM FIRST_KEYFRAME KEYFRAME_TRANSLATION TOT_TRANSLATI
 roi = [PATCHRADIUS+1,PATCHRADIUS+1,width-(2*PATCHRADIUS),height-(2*PATCHRADIUS)];
 newCorners = detectHarrisFeatures(image,'ROI',roi);
 
-% Desc2 = describeKeyPoints(image,corners2.Location(:,1),corners2.Location(:,2));
-
-% Match keypoints by calculating BRIEF descriptors and matching
-% [Match1, Match2, ~, Idx2] = matchDescriptors(prevState.Descriptors,Desc2,prevState.Keypoints',corners2.Location);
-% Descriptors = Desc2(:,Idx2);
-
 % KLT
 pointTracker = vision.PointTracker('MaxBidirectionalError',1); % Set to Inf for speedup
 initialize(pointTracker,prevState.Keypoints',prev_image);
@@ -47,11 +40,6 @@ initialize(pointTracker,prevState.Keypoints',prev_image);
 [trackedPoints,trackedPointsValidity] = pointTracker(image);
 KLTMatch1 = prevState.Keypoints(:,trackedPointsValidity)';
 KLTMatch2 = trackedPoints(trackedPointsValidity,:);
-
-% Estimate relative pose between initial frames and create 3D pointcloud
-% Check if det(F) = 0, if not correct as in Ex. 6 (Simon)
-% F = estimateFundamentalMatrix(Match1, Match2, 'Method', ...
-%                               'RANSAC', 'NumTrials', 2000);
                           
 F_KLT = estimateFundamentalMatrix(KLTMatch1, KLTMatch2, 'Method', ...
                               'RANSAC', 'NumTrials', 2000);
@@ -59,8 +47,8 @@ F_KLT = estimateFundamentalMatrix(KLTMatch1, KLTMatch2, 'Method', ...
 % Recover essential matrix from F, then decompose into R,T
 E = K'*F_KLT*K;
 [Rots,u3] = decomposeEssentialMatrix(E);
-KLTMatch1(:,3)=1; % TODO: These must be the homogeneous coords of the matches
-KLTMatch2(:,3)=1; % TODO: These must be the homogeneous coords of the matches
+KLTMatch1(:,3)=1;
+KLTMatch2(:,3)=1;
 [R,T] = disambiguateRelativePose(Rots,u3,KLTMatch1',KLTMatch2',K,K);
 
 Keypoints = KLTMatch2(:,1:2)';
@@ -81,52 +69,32 @@ PointsNoMatch = PointsNoMatch(posIdxs,:);
 % Check if there are any candidate keypoints yet (only empty directly after
 % keyframe)
 if isempty(prevState.CandidateKeypoints)
-%     curState.CandidateKeypoints = corners2.Location(~trackedPointsValidity,:)';
-%     curState.CandidateDescriptors = Desc2(:,~trackedPointsValidity);
-%     curState.InitCandidateKeypoints = corners2.Location(~trackedPointsValidity,:)';
-%     curState.InitCandidatePoses = repmat(curPose(:),1,sum(~trackedPointsValidity));
     % Add newly detected corners that aren't duplicates
     newCornersMask = findNewCandidates(newCorners.Location,PointsNoMatch);
     newDetectedCorners = newCorners.Location(newCornersMask,:);
     curState.CandidateKeypoints = [PointsNoMatch' newDetectedCorners'];
-    curState.CandidateDescriptors = [];
     curState.InitCandidateKeypoints = [PointsNoMatch' newDetectedCorners'];
     curState.InitCandidatePoses = repmat(curPose(:),1,size(PointsNoMatch,1)+size(newDetectedCorners,1));
 else
     % Track candidate keypoints and update candidate list
-%     DescNoMatch = Desc2(:,~Idx2);
-%     PointsNoMatch = corners2.Location(~Idx2,:);
-%     DescNoMatch = Desc2(:,~trackedPointsValidity);
     candPointTracker = vision.PointTracker('MaxBidirectionalError',1); % Set to Inf for speedup
     initialize(candPointTracker,prevState.CandidateKeypoints',prev_image);
     [trackedCandPoints,trackedCandPointsValidity] = candPointTracker(image);
     uniqueNewIdxs = findNewCandidates(PointsNoMatch,trackedCandPoints);
     
-%     [~, ~, CandIdx1, CandIdx2] = matchDescriptors(prevState.CandidateDescriptors,DescNoMatch,prevState.CandidateKeypoints',PointsNoMatch);
     % First only keep candidates that were succesfully tracked and update
     % non-init components
-%     newCandidateKeypoints = PointsNoMatch(CandIdx2,:)';
-%     newCandidateDescriptors = DescNoMatch(:,CandIdx2);
-%     newInitCandidateKeypoints = prevState.InitCandidateKeypoints(:,CandIdx1);
-%     newInitCandidatePoses = prevState.InitCandidatePoses(:,CandIdx1);
     newCandidateKeypoints = trackedCandPoints(trackedCandPointsValidity,:)';
-    newCandidateDescriptors = [];
     newInitCandidateKeypoints = prevState.InitCandidateKeypoints(:,trackedCandPointsValidity);
     newInitCandidatePoses = prevState.InitCandidatePoses(:,trackedCandPointsValidity);
     % Then append new candidates
-%     newCandidateKeypoints = [newCandidateKeypoints PointsNoMatch(~CandIdx2,:)'];
-%     newCandidateDescriptors = [newCandidateDescriptors DescNoMatch(:,~CandIdx2)];
-%     newInitCandidateKeypoints = [newInitCandidateKeypoints PointsNoMatch(~CandIdx2,:)'];
-%     newInitCandidatePoses = [newInitCandidatePoses repmat(curPose(:),1,sum(~CandIdx2))];
     newCornersMask = findNewCandidates(newCorners.Location,PointsNoMatch(uniqueNewIdxs,:));
     newDetectedCorners = newCorners.Location(newCornersMask,:);
     newCandidateKeypoints = [newCandidateKeypoints PointsNoMatch(uniqueNewIdxs,:)' newDetectedCorners'];
-    newCandidateDescriptors = [];
     newInitCandidateKeypoints = [newInitCandidateKeypoints PointsNoMatch(uniqueNewIdxs,:)' newDetectedCorners'];
     newInitCandidatePoses = [newInitCandidatePoses repmat(curPose(:),1,sum(uniqueNewIdxs)+sum(newCornersMask))];
     % Write updated candidates to state
     curState.CandidateKeypoints = newCandidateKeypoints;
-    curState.CandidateDescriptors = newCandidateDescriptors;
     curState.InitCandidateKeypoints = newInitCandidateKeypoints;
     curState.InitCandidatePoses = newInitCandidatePoses; %does this overwrite also init pose of all canditate or only the new?
 end
@@ -142,13 +110,9 @@ TOT_TRANSLATION=TOT_TRANSLATION+T;
 if (isKeyFrame(curState.Landmarks)) || (FRAME_NUM == FIRST_KEYFRAME)
     disp('Keyframeframe');
     curState.Keypoints = Keypoints;
-%     curState.Descriptors = Descriptors;
-    curState.Descriptors = [];
     [comp, nvec] = triangNewKPoint(curState,R);
     if ~all(comp == 0)
         correspondCandidate=curState.CandidateKeypoints(:,comp>0);
-%         correspondCandidateDescriptors=curState.CandidateDescriptors(:,comp>0);
-        correspondCandidateDescriptors=[];
         validCand=curState.InitCandidatePoses(:,comp>0);
         nvec=nvec(:,comp>0);
         nvec(3,:)=1;
@@ -161,13 +125,10 @@ if (isKeyFrame(curState.Landmarks)) || (FRAME_NUM == FIRST_KEYFRAME)
         %eliminate triangulated from candidate
         curState.InitCandidatePoses=curState.InitCandidatePoses(:,comp>0);
         curState.CandidateKeypoints=curState.CandidateKeypoints(:,comp>0);
-%         curState.CandidateDescriptors=curState.CandidateDescriptors(:,comp>0);
-        curState.CandidateDescriptors=[];
         curState.InitCandidateKeypoints=curState.InitCandidateKeypoints(:,comp>0);
         %curState.InitCandidatePoses=curState.InitCandidatePoses(:,comp>0); alerady cut??
         curState.Landmarks=[curState.Landmarks,newLand(1:3,:)];
         curState.Keypoints=[curState.Keypoints,correspondCandidate];
-        curState.Descriptors=[curState.Descriptors,correspondCandidateDescriptors];
     end
 end
 
