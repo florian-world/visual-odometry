2e6303857d0fb158163aae8a6bab453d7c7f72c4
CNesso <djnesso@gmail.com>
2020-01-03 15:20:20 +0100

    solved traking of same frame

--- code/triangNewKPoint.m
+++ code/triangNewKPoint.m
@@ -8,9 +8,11 @@ function [comp, nvec] =triangNewKPoint(curState,R)
         nvec(:,i)=uvToRotVec(curState.CandidateKeypoints(:,i))';
         %nrot=firstRcw*rotationVectorToMatrix(nvec);
         candPose=reshape(curState.InitCandidatePoses(:,i),[3,4]);
+        initNvec(:,i)=uvToRotVec(curState.InitCandidateKeypoints(:,i))';
         candRot=candPose(:,1:3);
-        nrot=candRot*rotationVectorToMatrix(nvec(:,i));
-        if dist(quaternion(nrot,'rotmat','frame'),quaternion(R,'rotmat','frame'))>MAGIC_KEYFRAME_ANGLE_RAD
+        initNrot=candRot*rotationVectorToMatrix(initNvec(:,i));
+        nrot=R*rotationVectorToMatrix(nvec(:,i));
+        if dist(quaternion(nrot,'rotmat','frame'),quaternion(initNrot,'rotmat','frame'))>MAGIC_KEYFRAME_ANGLE_RAD
             comp(i)=true;
            %add to totriangulate
         end
